47b8957ebbd620eaca14790017460c7452ebe423
Sherwood <shcallaway@gmail.com>
2017-04-17 21:04:39 -0700

    Add aws rds ssl in database config

--- server/database.js
+++ server/database.js
@@ -8,7 +8,8 @@ const connection = mysql.createConnection({
   host: process.env.HOST,
   user: process.env.USERNAME,
   password: process.env.PASSWORD,
-  database: process.env.DATABASE
+  database: process.env.DATABASE,
+  ssl: 'Amazon RDS'
 }); 
     
 module.exports = connection
